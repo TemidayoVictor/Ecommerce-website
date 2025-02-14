f4802aacda026e6a742c9d22f9e826164c3fb3f8
TemidayoVictor <temidayovictor1@gmail.com>
2025-02-15 00:13:13 +0100

    Home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2000,6 +2000,108 @@
                     </div>
                 </div>
             </section>
+
+            <section class="newsletter section home__newsletter">
+                <div class="newsletter__container container grid">
+                    <h3 class="newsletter__title flex">
+                        <img src="{{ asset('assets/icon-email.svg') }}" class="newsletter__icon" alt="">
+                        Sign up to Newsletter
+                    </h3>
+                    <p class="newsletter__description">
+                        ... and receive $25 coupon for shopping
+                    </p>
+                    <form action="" class="newsletter__form">
+                        <input 
+                            type="text" 
+                            placeholder="Enter your email" 
+                            class="newsletter__input"
+                        >
+                        <button type="submit" class="newsletter__btn">Subscribe</button>
+                    </form>
+                </div>
+            </section>
+
+            <footer class="footer container">
+                <div class="footer__container grid">
+                    
+                    <div class="footer__content">
+                        <a href="" class="footer__logo">
+                            <img src="{{ asset('assets/logo.svg') }}" alt="" class="footer__logo-img">
+                        </a>
+                        <h4 class="footer__subtitle">Contact</h4>
+                        
+                        <p class="footer__description">
+                            <span>Address: </span> 562 Wellington Road, Street 32, Sanfrancisco
+                        </p>
+
+                        <p class="footer__description">
+                            <span>Phone: </span> (+234) 8139943234
+                        </p>
+
+                        <p class="footer__description">
+                            <span>Hours: </span> 10:00 - 18:00 Mon - Sat
+                        </p>
+
+                        <div class="footer__social">
+                            
+                            <h4 class="footer__subtitle">Follow Us</h4>
+
+                            <div class="footer__social-links flex-1">
+                                
+                                <a href="" class="footer__logo">
+                                    <img src="{{ asset('assets/icon-facebook.svg') }}" alt="" class="footer__social-icon">
+                                </a>
+
+                                <a href="" class="footer__logo">
+                                    <img src="{{ asset('assets/icon-twitter.svg') }}" alt="" class="footer__social-icon">
+                                </a>
+
+                                <a href="" class="footer__logo">
+                                    <img src="{{ asset('assets/icon-instagram.svg') }}" alt="" class="footer__social-icon">
+                                </a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="footer__content">
+                        <h3 class="footer__title">Address</h3>
+                        <ul class="footer__links">
+                            <li> <a href="" class="footer__link">About Us</a> </li>
+                            <li> <a href="" class="footer__link">Delivery Information</a> </li>
+                            <li> <a href="" class="footer__link">Privacy Policy</a> </li>
+                            <li> <a href="" class="footer__link">Terms & Conditions</a> </li>
+                            <li> <a href="" class="footer__link">Contact Us</a> </li>
+                            <li> <a href="" class="footer__link">Support Center</a> </li>
+                        </ul>
+                    </div>
+
+                    <div class="footer__content">
+                        <h3 class="footer__title">My Account</h3>
+                        <ul class="footer__links">
+                            <li> <a href="" class="footer__link">Sign In</a> </li>
+                            <li> <a href="" class="footer__link">View Cart</a> </li>
+                            <li> <a href="" class="footer__link">My Wishlist</a> </li>
+                            <li> <a href="" class="footer__link">Track My Order</a> </li>
+                            <li> <a href="" class="footer__link">Help</a> </li>
+                            <li> <a href="" class="footer__link">Order</a> </li>
+                        </ul>
+                    </div>
+
+                    <div class="footer__content">
+                        <h3 class="footer__title">Secure Payments Gateways</h3>
+                        <img src="{{ asset('assets/payment-method.png') }}" alt="" class="payment__img">
+                    </div>
+
+                </div>
+
+                <div class="footer__bottom">
+                    <p class="copyright">&copy; 2023 Evara. All Rights Reserved</p>
+                    <span class="designer">Designed by Developers</span>
+                </div>
+            </footer>
         </main>
 
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>

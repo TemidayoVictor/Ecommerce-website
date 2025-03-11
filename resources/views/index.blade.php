310f5128e84b10d2f4feef8ca714c889f585f2d8
Adeboye Daniel <adeboyedaniel3@gmail.com>
2025-03-11 16:32:58 +0100

    Homepage_cart

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -128,7 +128,7 @@ Home
                                 <span class="old__price">$245.8</span>
                             </div>
 
-                            <a href="" class="action__btn cart__btn" aria-label="Add To Cart" data-product-id="{{ $product->id }}">
+                            <a href="" class="action__btn cart__btn add__btn" aria-label="Add To Cart" data-product-id="{{ $product->id }}">
                                 <i class="ri-shopping-cart-2-line"></i>
                             </a>
                         </div>
